22e83db266d0f0e4507b2b8381240f66ecff6c69
Jaydeepwakade <jaydeepwakade123@gmail.com>
2024-06-24 14:17:55 +0530

    login page design

--- src/Componets/Login_&_Signup/Login/Login.jsx
+++ src/Componets/Login_&_Signup/Login/Login.jsx
@@ -3,6 +3,10 @@ import { useNavigate } from "react-router-dom";
 import Art from "../../../assets/Art.png";
 import Back from "../../../assets/Back.png";
 import style from "./Login.module.css";
+import Group from "../../../assets/Group.svg";
+import icon from "../../../assets/icon.svg";
+import view from "../../../assets/view.svg";
+
 
 function Login() {
   const navigate = useNavigate();
@@ -16,20 +20,38 @@ function Login() {
 
   return (
     <div className={style.container}>
-
       <div className={style.leftDiv}>
-
         <div className={style.imagecontainer}>
           <img className={style.backImage} src={Back} alt="" />
           <img className={style.artImage} src={Art} alt="Art" />
         </div>
-
         <h2>Welcome aboard my friend</h2>
         <p>just a couple of clicks and we start</p>
       </div>
-      <div>
-        <button onClick={handleLogin}>Login</button>
-        <button onClick={handlesignup}>signup</button>
+
+      <div className={style.loginDiv}>
+        <h2>Login</h2>
+        <form action="">
+          <div className={style.mainDiv}>
+            <div className={style.inputDiv}>
+              <span>
+                <img src={icon} alt="icon" />
+              </span>
+              <input placeholder="Email" type="email" />
+            </div>
+            <div className={style.inputDiv}>
+              <span>
+                <img src={Group} alt="lock" />
+              </span>
+              <input placeholder="Password" type="password" />
+             <span><img src={view} alt="view" /></span> </div>
+          </div>
+        </form>
+        <div className={style.btndiv}>
+            <button onClick={handleLogin}>Login</button>
+            <p>Have no account yet ?</p>
+            <button onClick={handlesignup}>signup</button>
+          </div>
       </div>
     </div>
   );
