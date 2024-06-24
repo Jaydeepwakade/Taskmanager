ad0c753ac959c32e4325e3a2543eda1860d44301
Jaydeepwakade <jaydeepwakade123@gmail.com>
2024-06-24 19:09:19 +0530

    ui desgin

--- src/Componets/Dasboardpages/anylacticspage/Anylactics.jsx
+++ src/Componets/Dasboardpages/anylacticspage/Anylactics.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Style from "./anyalis.module.css";
+import Ellipse3 from "../../../assets/Ellipse3.svg"
 
 
 function Anylactics() {
+  const [taskcountlist, settaskcountlist] = useState([
+    "Backlog Tasks",
+    "Low Priority",
+    "To-do Tasks",
+    "Moderate Priority",
+    "in-Progress Tasks",
+    "High Priority",
+    "Completed tasks",
+    "Due Date Tasks",
+  ]);
   return (
-    <div>
-     ANylactics
+    <div className={Style.container}>
+      <div className={Style.maindiv}>
+
+        {taskcountlist.map((ele)=><div><img src={Ellipse3} alt="" /> {ele}</div>)}
+      </div>
     </div>
   );
 }
