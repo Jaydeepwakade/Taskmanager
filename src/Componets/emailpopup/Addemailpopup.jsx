fd087b3829c60f201962b57a7223edef27903014
Jaydeepwakade <jaydeepwakade123@gmail.com>
2024-06-29 01:04:04 +0530

    desgin done

--- src/Componets/emailpopup/Addemailpopup.jsx
+++ src/Componets/emailpopup/Addemailpopup.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import styles from './ConfirmationModal.module.css';
+import React, { useState } from 'react';
+import styles from './addpopup.module.css';
 
 const AddEmailpopup = ({ isOpen, message, onClose, onConfirm,buttontxt}) => {
+const [email,setEmail]=useState('')
+ console.log(email)
   return (
     <>
       {isOpen && (
         <div className={styles.modalOverlay}>
           <div className={styles.modalContent}>
-            <p>{message}</p>
+            <p>Add people to the Board</p>
+
+            <input onChange={(e)=>setEmail(e.target.value)} type="text" placeholder='Enter the email' />
             <div className={styles.buttons}>
-            <button onClick={onConfirm} className={styles.confirmButton}>
-                {buttontxt}
-              </button>
-              <button onClick={onClose} className={styles.cancelButton}>
+            <button onClick={onClose} className={styles.cancelButton}>
                 Cancel
               </button>
+            <button onClick={onConfirm} className={styles.confirmButton}>
+               Add Email
+              </button>
+             
               
             </div>
           </div>
